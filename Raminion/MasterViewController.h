b68a026d8d067a9bc3417c2c3ee4b520e74b6569
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-15 17:19:43 -0700

    add new class to deal with gui.

---
+++ Raminion/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/15/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
---
+++ Raminion/MasterViewController.m
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.m
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/15/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
