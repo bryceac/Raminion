60c41276a5f761ae686e91e8ed0d5dd447493cff
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-16 09:22:58 -0700

    add class to deal with cards.

---
+++ Raminion/Cards.h
@@ -0,0 +1,22 @@
+//
+//  Cards.h
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/16/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cards : NSObject
+{
+    NSMutableArray* cards;
+}
+
+- (void)createCards:(NSDictionary*)s;
+
+- (void)shuffle;
+
+- (NSMutableArray*)cards;
+
+@end
---
+++ Raminion/Cards.m
@@ -0,0 +1,13 @@
+//
+//  Cards.m
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/16/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import "Cards.h"
+
+@implementation Cards
+
+@end
