9446c7f68299d7dc705c1bf8511a5ab5bc1d00e7
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-16 09:26:43 -0700

    add class to do shuffling.

---
+++ Raminion/Shuffle.h
@@ -0,0 +1,13 @@
+//
+//  Shuffle.h
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/16/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shuffle : NSObject
+
+@end
---
+++ Raminion/Shuffle.m
@@ -0,0 +1,13 @@
+//
+//  Shuffle.m
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/16/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import "Shuffle.h"
+
+@implementation Shuffle
+
+@end
