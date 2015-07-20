8f47b817966658066bae1a44ba675da774591ced
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-19 17:11:53 -0700

    add new view so set selection is possible.

---
+++ Raminion/SetListController.h
@@ -0,0 +1,13 @@
+//
+//  SetListController.h
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/19/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SetListController : NSViewController
+
+@end
---
+++ Raminion/SetListController.m
@@ -0,0 +1,22 @@
+//
+//  SetListController.m
+//  Raminion
+//
+//  Created by Bryce Campbell on 7/19/15.
+//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//
+
+#import "SetListController.h"
+
+@interface SetListController ()
+
+@end
+
+@implementation SetListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
