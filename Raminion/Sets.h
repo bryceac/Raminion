35e7eb8b0d297538851ad6379d8f0c63a93e02d7
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-19 22:11:00 -0700

    modify copyright notices and put project under the MIT License.

--- Raminion/Sets.h
+++ Raminion/Sets.h
@@ -3,7 +3,7 @@
 //  Raminion
 //
 //  Created by Bryce Campbell on 7/19/15.
-//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//  Copyright (c) 2015 Bryce Campbell.
 //
 
 #import <Foundation/Foundation.h>
--- Raminion/SetupViewController.h
+++ Raminion/SetupViewController.h
@@ -3,7 +3,7 @@
 //  Raminion
 //
 //  Created by Bryce Campbell on 7/18/15.
-//  Copyright (c) 2015 Bryce Campbell. All rights reserved.
+//  Copyright (c) 2015 Bryce Campbell.
 //
 
 #import <Cocoa/Cocoa.h>
