f179cb418dc32c328caa1743d8a39befaf63c8c8
Bryce Campbell <tonyhawk2100@gmail.com>
2015-07-23 08:02:27 -0700

    try different kind of nested loop.

--- Raminion/Game.m
+++ Raminion/Game.m
@@ -21,79 +21,89 @@
 - (int) supply:(NSMutableArray*)c bane:(Card *)b
 {
     int events = 0; // variable to count number of events encountered
+    int supply = 0; //variable to count supply
     
     // check for bane card, in order to determine decks in supply
     if (b != nil) {
-        for (int i = 0; i < [c count]; i++)
-        {
-            if ([c[i] event] == true && events < 2)
+        for (int h = 0; h < 11; h++) {
+            for (int i = 0; i < [c count]; i++)
             {
-                events++;
-                [eventCards addObject:c[i]];
-            }
-            else
-            {
+                if ([c[i] event] == true && events < 2)
+                {
+                    events++;
+                    [eventCards addObject:c[i]];
+                }
+                else if ([c[i] event] == true)
+                {
                     
+                }
+                else
+                {
+                    supply = h+1;
+                }
             }
         }
         
+        supply += events;
         
-        if (events == 2)
+        
+        if (supply == 13)
         {
             int index = [c indexOfObject:b];
             [c exchangeObjectAtIndex:index withObjectAtIndex:12];
             
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[0]] withObjectAtIndex:11];
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[1]] withObjectAtIndex:10];
-            return 13;
-            
         }
-        else if (events == 1)
+        else if (supply == 12)
         {
             int index = [c indexOfObject:b];
             [c exchangeObjectAtIndex:index withObjectAtIndex:11];
             
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[0]] withObjectAtIndex:10];
-            return 12;
         }
         else
         {
             int index = [c indexOfObject:b];
             [c exchangeObjectAtIndex:index withObjectAtIndex:10];
-            return 11;
         }
     }
     else
     {
-        for (int i = 0; i < [c count]; i++)
-        {
-            if ([c[i] event] == true && events < 2)
+        for (int h = 0; h < 10; h++) {
+            for (int i = 0; i < [c count]; i++)
             {
-                events++;
-                [eventCards addObject:c[i]];
-            }
-            else
-            {
-                
+                if ([c[i] event] == true && events < 2)
+                {
+                    events++;
+                    [eventCards addObject:c[i]];
+                }
+                else if ([c[i] event] == true)
+                {
+                    
+                }
+                else
+                {
+                    supply = h+1;
+                }
             }
         }
         
-        if (events == 2)
+        supply += events;
+        
+        if (supply == 12)
         {
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[0]] withObjectAtIndex:11];
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[1]] withObjectAtIndex:10];
-            return 12;
-            
         }
-        else if (events == 1)
+        else if (supply == 11)
         {
             [c exchangeObjectAtIndex:[c indexOfObject:eventCards[0]] withObjectAtIndex:10];
-            return 11;
         }
         else
         {
         }
     }
-    return 10;
+    return supply;
 }
 @end
